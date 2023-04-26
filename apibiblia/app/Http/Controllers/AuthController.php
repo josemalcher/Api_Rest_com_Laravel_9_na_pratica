88e5542fa105693dad26aad87b977274b08eaf36
josemalcher <contato@josemalcher.net>
2023-04-26 16:57:01 -0300

    14. Login

--- apibiblia/app/Http/Controllers/AuthController.php
+++ apibiblia/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -21,6 +22,31 @@ class AuthController extends Controller
             ]);
 
         $token = $user->createToken($request->nameToken)->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'E-mail ou Senha invalidos.'
+            ], 401);
+        }
+
+        $token = $user->createToken('UsuarioLogado')->plainTextToken;
 
         $response = [
             'user' => $user,
--- apibiblia/routes/api.php
+++ apibiblia/routes/api.php
@@ -50,6 +50,7 @@ Route::apiResources([
 ]);
 
 Route::post('/register' , [AuthController::class, 'register']);
+Route::post('/login' , [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
